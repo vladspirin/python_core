a7588d2755a93d49e9508fad41b3d65c4dcc3bda
Vladyslav <vladspirin@hotmail.com>
2021-01-21 17:28:06 +0200

    Add hw4

---
+++ Module4_HW/hw4_01.py
@@ -0,0 +1,10 @@
+s = """
+Beautiful is better than ugly
+Explicit is better than implicit
+Simple is better than complex
+Complex is better than complicated
+Readability counts
+"""
+print(s)
+print(s.upper())
+print(s.lower())
---
+++ Module4_HW/hw4_02.py
@@ -0,0 +1,4 @@
+number = input("Please enter your 4-digit natural number: ")
+n1 = int(number[0]) * int(number[1]) * int(number[2]) * int(number[3])
+print(n1)
+print(int(number[::-1]))
