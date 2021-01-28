1866b1319abacf29d484450bf02f1562d0bb962d
Vladyslav <vladspirin@hotmail.com>
2021-01-28 16:53:00 +0200

    Update task 5

--- Module5_HW/task5.py
+++ Module5_HW/task5.py
@@ -1,8 +1,8 @@
 sup_num_list = ['x\u00b9', 'x\u00b2', 'x\u00b3', 'x\u2074'] 
 for x in sup_num_list:
-    print(x, end='    ')
-print('')
+    print("%10s" % x, end='')
+print()
 for i in range(1, 10):    
     for j in range(1, 5):
-        print(pow(i, j), end='     ')
-    print('')
+        print("%10d" % i ** j, end='')
+    print()
