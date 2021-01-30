97b8f17bb3e1a792c177cf0b61c4781296ea7e55
Vladyslav <vladspirin@hotmail.com>
2021-01-30 17:38:53 +0200

    Add hw6_1_1

--- Module6_HW/hw6_1_1.py
+++ Module6_HW/hw6_1_1.py
@@ -1,1 +1,15 @@
+from random import randint
 
+# 1
+n = int(input('Input the length of the list: '))
+numbers = [randint(1, 100) for i in range(n)]
+print(f'Our random list: {numbers}')
+
+# 2
+n1 = int(input('Input the length of the list: '))
+numbers2 = [int(input(f'Input {j} index number: ')) for j in range(n1)]
+print(f'Our manual entered list: {numbers2}')
+
+# 3
+numbers3 = [sum(numbers), sum(numbers2)]
+print(f'Our new list with sum of numbers: {numbers3}')
