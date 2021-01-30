9997b52fa92d79aa520b4162bcc4d48984c68de0
Vladyslav <vladspirin@hotmail.com>
2021-01-30 16:55:22 +0200

    Add task with fix and correct result

--- Module6_HW/task4.py
+++ Module6_HW/task4.py
@@ -1,27 +1,51 @@
 from random import randint
 
 # 1
+print('# 1')
 col = int(input('Please enter the number of columns: '))
 row = int(input('Please enter the number of rows: '))
-temp = [randint(1, 20) for i in range(col)]
-print(temp)
-my_list = []
+numbers = [[0] * col for row in range(row)]
 for i in range(row):
     for j in range(col):
-        ro = [randint(1, 20) for i in range(col)]
-        my_list.append(ro)
-        print("%4d" % my_list[i][j], end="")
+        numbers[i][j] = randint(1, 100)
+        print("%4d" % numbers[i][j], end="")
     print()
+
 # 2
+print('# 2')
+print(numbers)
 # min row3 element and max column2
-print(my_list)
-for r in my_list:
-    for c in r:
-        if r == 2:
-            print(min(r))
-        # print(c, end=' ')
+temp = []
+for j in range(col):
+    temp.append(numbers[2][j])
+print('The min element in the 3rd row: %4d' % min(temp))
+print(temp)
+temp = []
+for i in range(row):
+    temp.append(numbers[i][1])
+print('The max element in the 2nd column: %4d' % max(temp))
+print(temp)
+
 # 3
+print('# 3')
+print('Initial list: ')
+for i in range(row):
+    for j in range(col):
+        print("%4d" % numbers[i][j], end="")
+    print()
+
+for i in range(row):
+    tmp = numbers[i][3]
+    numbers[i][3] = numbers[i][1]
+    numbers[i][1] = tmp
+
+print('Changed list: ')
+for i in range(row):
+    for j in range(col):
+        print("%4d" % numbers[i][j], end="")
+    print()
 
 # 4
-
-# table = [[0] * row for і in range(column)]
+print('# 4')
+sum_of_diagonal = sum([numbers[i][j] for i in range(row) for j in range(col) if i == j])
+print("The sum of diagonal is %4d" % sum_of_diagonal, end="")
