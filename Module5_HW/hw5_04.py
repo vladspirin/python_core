4b5dc9fb5426ceb95772b1e21da5baeab2967eab
Vladyslav <vladspirin@hotmail.com>
2021-01-31 16:46:43 +0200

    Not complete, just switch to another OS

---
+++ Module5_HW/hw5_04.py
@@ -0,0 +1,10 @@
+symbol_1 = '#'
+symbol_2 = '*'
+a = int(input('Input side A: '))
+b = int(input('Input side B: '))
+for i in range(a):
+    for j in range(b):
+        print("%s " % symbol_1, end="")
+    print()
+
+# in process.....
