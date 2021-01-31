0bc9fd7050f42d3f161a04205252723629885e8c
Vladyslav <vladspirin@hotmail.com>
2021-01-31 17:49:00 +0200

    Add hw5_01

--- Module5_HW/hw5_01.py
+++ Module5_HW/hw5_01.py
@@ -2,8 +2,16 @@ import random
 
 
 rnd = random.randint(0, 100)
-print(rnd)
-number = int(input("Please enter your number: "))
+print("You have 10 attempts. Please guess the number.")
+
 for i in range(0, 10):
-    if i < number:
-        print("")
+    number = int(input("Please enter your number: "))
+    if rnd == number:
+        print("You guessed the number.")
+        break
+    elif rnd < number:
+        print("You entered a higher number.")
+    elif rnd > number:
+        print("You entered a lower number.")
+else:
+    print(f"You didn't guess the number. The number is {rnd}")
