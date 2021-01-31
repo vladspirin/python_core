418aaca86875f48e011fb4263b8372b11d84cfa2
Vladyslav <vladspirin@hotmail.com>
2021-01-31 17:17:29 +0200

    Add hw5_04

--- Module5_HW/hw5_04.py
+++ Module5_HW/hw5_04.py
@@ -1,10 +1,12 @@
-symbol_1 = '#'
-symbol_2 = '*'
-a = int(input('Input side A: '))
-b = int(input('Input side B: '))
-for i in range(a):
-    for j in range(b):
-        print("%s " % symbol_1, end="")
+length = int(input('Input side length: '))
+height = int(input('Input side height: '))
+for i in range(length):
+    if i == 0 or i == length - 1:
+        for j in range(height):
+            print('#', end='')
+    else:
+        print('#', end='')
+        for j in range(1, height - 1):
+            print('*', end='')
+        print('#', end='')
     print()
-
-# in process.....
