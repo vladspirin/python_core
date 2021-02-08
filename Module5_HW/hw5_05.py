60ea29f26a9104a801551699e7b115c723ccdf8c
Vladyslav <vladspirin@hotmail.com>
2021-02-08 16:27:18 +0200

    Add hw5_05

--- Module5_HW/hw5_05.py
+++ Module5_HW/hw5_05.py
@@ -1,4 +1,19 @@
-P = int(input("Please input P: "))
-H = int(input("Please input H: "))
+P = int(input("Input P: "))
+H = int(input("Input H: "))
+print("If you want to exit input number = P or number = H")
+sum_num = 0
+mul_num = 1
 counter = 0
-
+while True:
+    numbers = int(input("Input numbers: "))
+    if numbers == P or numbers == H:
+        break
+    if numbers < P:
+        sum_num += numbers
+    if numbers > H:
+        mul_num *= numbers
+    if P < numbers < H:
+        counter += 1
+print("Sum of num < P = %d " % sum_num)
+print("Mult of num > H = %d " % mul_num)
+print("Amount of numbers between P and H %d " % counter)
